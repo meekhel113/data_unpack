a5ed290361ab62e50314e54749153cd4c0f82682
Meekhel Patel <meekhelpatel@gmail.com>
2023-09-03 22:46:09 -0400

    Added first/last flags, and implemented gearbox into data unpack

--- gearbox.sv
+++ gearbox.sv
@@ -5,20 +5,25 @@ module data_unpack
     OUT_WIDTH = 7;
 )
 (
-  input wire clk,
-  input wire rst,     // Assuming active high reset
+  input   wire            clk,
+  input   wire            rst,     // Assuming active high reset
   
-  input wire valid_in,
-  input wire [31:0] data_in,
+  input   wire                      valid_in,
+  input   wire    [IN_WIDTH-1:0]    data_in,
 
-  output logic valid_out,
-  output logic [6:0] data_out,
+  output  logic                     valid_out,
+  output  logic   [OUT_WIDTH-1:0]   data_out,
+
+  output  logic                     first_packet,
+  output  logic                     last_packet
 );
 
-logic   [32+7-1:0]          build_reg;
-logic   [$clog2(32+7)-1:0]  valid_bits;
+logic   [IN_WIDTH+OUT_WIDTH-1:0]          build_reg;
+logic   [$clog2(IN_WIDTH+OUT_WIDTH)-1:0]  valid_bits;
 
-assign valid_out = valid_bits > 7;
+assign valid_out = valid_bits > OUT_WIDTH;
+assign first_packet = valid_bits > IN_WIDTH;
+assign last_packet = (valid_bits > OUT_WIDTH) && (valid_bits < 2*OUT_WIDTH);
 
 always_ff@(posedge clk)
 begin
@@ -26,9 +31,9 @@ begin
     valid_bits <= 0;
   else
     case({valid_out, valid_in})
-    2'b01: valid_bits <= valid_bits + 32;
-    2'b10: valid_bits <= valid_bits - 7;
-    2'b11: valid_bits <= valid_bits + 32 - 7;
+    2'b01: valid_bits <= valid_bits + IN_WIDTH;
+    2'b10: valid_bits <= valid_bits - OUT_WIDTH;
+    2'b11: valid_bits <= valid_bits + IN_WIDTH - OUT_WIDTH;
     endcase
 end
 
@@ -39,9 +44,9 @@ begin
     data_out <= 0;
   else
     if(valid_in)
-      build_reg <= (build_reg << 32) & data_in
+      build_reg <= {(build_reg << IN_WIDTH), data_in};
     else if(valid_out)
-      {data_out, build_reg} <= build_reg << 7;
+      {data_out, build_reg} <= build_reg << OUT_WIDTH;
 end
 
 
